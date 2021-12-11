7b981d75a8a2eeb83df31caed5f36f7b74437b13
Bevin Sunth <r.bevinsunth@gmail.com>
2021-12-11 16:01:27 +1100

    Updated mock data

--- PrettyPage.DataAccess/Repositories/ContentAccessRepository.cs
+++ PrettyPage.DataAccess/Repositories/ContentAccessRepository.cs
@@ -10,19 +10,20 @@ namespace PrettyPage.DataAccess.Repositories
         public HomePage GetHomePageData()
         {
             List<CarousalImage> carosolImages = new List<CarousalImage>();
-            carosolImages.Add(new CarousalImage { ImageUrl = "https://picsum.photos/id/1/200/300", Text = "Image 1" });
-            carosolImages.Add(new CarousalImage { ImageUrl = "https://picsum.photos/id/2/200/300", Text = "Image 2" });
+            carosolImages.Add(new CarousalImage { ImageUrl = "https://picsum.photos/id/1/200/300", Text = "Work Time" });
+            carosolImages.Add(new CarousalImage { ImageUrl = "https://picsum.photos/id/2/200/300", Text = "Journal Time" });
 
             List<NavigationLink> navigationLinks = new List<NavigationLink>();
-            navigationLinks.Add(new NavigationLink { TargetUrl = "/Link 1", Text = "Link 1" });
-            navigationLinks.Add(new NavigationLink { TargetUrl = "/Link 2", Text = "Link 2" });
+            navigationLinks.Add(new NavigationLink { TargetUrl = "/Home", Text = "Home" });
+            navigationLinks.Add(new NavigationLink { TargetUrl = "/Pretty Stuff", Text = "Pretty Stuff" });
+            navigationLinks.Add(new NavigationLink { TargetUrl = "/Pretty Cool Stuff", Text = "Cool Stuff" });
 
             return new HomePage()
             {
                 CarousalImages = carosolImages,
                 NavigationLinks = navigationLinks,
                 IntroHeading = "Lorem Ipsum",
-                IntroText = "Lorem Ipsum"
+                IntroText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id sapien dapibus, cursus odio et, pulvinar lorem. Cras arcu turpis, interdum non tortor in, dapibus venenatis mi. Sed libero justo, gravida a rutrum nec, accumsan vitae dui. Proin non enim consectetur dui rhoncus suscipit. Proin elementum commodo tellus at fringilla. Vivamus vehicula eros sed magna feugiat venenatis. Vestibulum aliquet neque tellus, non efficitur ex rutrum eu. Donec in luctus magna. Morbi a velit eu arcu accumsan molestie sit amet quis sem. Cras accumsan, ipsum pellentesque auctor commodo, elit libero luctus nulla, in faucibus mauris felis et arcu. Nam dictum nisi non volutpat sodales. Vestibulum felis nulla, elementum sed est vitae, elementum lobortis purus. Curabitur fermentum diam in feugiat bibendum. Vestibulum purus est, pulvinar id imperdiet vel, sodales vitae nunc."
             };
         }
     }
